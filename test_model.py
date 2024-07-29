5e26fa0ecdc60b127e8e08a88d148b4b9d216c3d
Dante <francescopolymathes@gmail.com>
2024-07-29 11:57:25 +0200

    Update

--- test_model.py
+++ test_model.py
@@ -6,8 +6,9 @@ from qiskit.circuit.library import TwoLocal
 from qiskit.utils import QuantumInstance
 from qiskit.quantum_info import Operator
 from qiskit.opflow import I, X, Z
-from scipy.integrate import solve_ivp, quad
-from scipy.optimize import brentq
+from scipy.integrate import quad
+from scipy.optimize import minimize
+from scipy.optimize import differential_evolution
 
 def hamiltonian():
     H = (X ^ X) + (Z ^ Z)
@@ -40,60 +41,39 @@ def run_vqe():
     result = vqe.compute_minimum_eigenvalue(operator=operator)
     return result
 
-def ads_metric(z, d):
-    return 1 / (z * z)
+def ads_metric(z, d, parameters):
+    return np.sqrt(1 + np.sum([(p * z)**2 for p in parameters]))
 
-def equations_of_motion(s, y, d):
-    z, dz_ds = y
-    d2z_ds2 = -(d - 1) * z * dz_ds**2 / (1 + z**2 * dz_ds**2) + (d - 1) / z
-    return [dz_ds, d2z_ds2]
+def dz_dz(z, params):
+    return np.array([p * z for p in params])
 
-def shoot(z0, d, l):
-    def event(s, y):
-        return y[0] - l
-    event.terminal = True
+def minimal_surface_area(params, z_min, z_max, d):
+    def integrand(z):
+        dz_dz_values = dz_dz(z, params)
+        metric = ads_metric(z, d, params)
+        return metric * np.sqrt(1 + np.sum(dz_dz_values**2)) * z**(d-2)
     
-    sol = solve_ivp(lambda s, y: equations_of_motion(s, y, d), [0, 100], [z0, 0], 
-                    events=event, dense_output=True)
-    return sol.t[-1], sol
-
-def find_minimal_surface(d, l):
-    def f(z0):
-        s_max, sol = shoot(z0, d, l)
-        return sol.y[1, -1]
-    
-    z0 = brentq(f, 1e-10, l)
-    s_max, sol = shoot(z0, d, l)
-    return sol
-
-def compute_area(sol, d):
-    def integrand(s):
-        z = sol.sol(s)[0]
-        dz_ds = sol.sol(s)[1]
-        return (z ** (1-d)) * np.sqrt(1 + dz_ds**2)
-    
-    area, _ = quad(integrand, 0, sol.t[-1])
+    area, _ = quad(integrand, z_min, z_max, epsabs=1.0e-8, epsrel=1.0e-8)
     return area
 
-def calculate_hee(d, l):
-    sol = find_minimal_surface(d, l)
-    area = compute_area(sol, d)
-    normalization = 1 / (4 * (d - 2))
+def compute_entropy(area):
+    G_N = 1.0
+    return area / (4 * G_N)
+
+def calculate_hee(d):
+    z_min = 0.1
+    z_max = 1.0
+    initial_params = [1.0] * 3
+    bounds = [(0, 5)] * len(initial_params)
+    result = differential_evolution(lambda p: minimal_surface_area(p, z_min, z_max, d), bounds)
     
-    return normalization * area
+    optimal_params = result.x
+    area = minimal_surface_area(optimal_params, z_min, z_max, d)
+    entropy = compute_entropy(area)
+    return entropy
 
 quantum_result = run_vqe()
-hee_result = calculate_hee(d=5, l=1.0)
+hee_result = calculate_hee(d=5)
 
 print("Quantum result:", quantum_result)
 print("Holographic Entanglement Entropy:", hee_result)
-
-dimensions = [3, 4, 5]
-strip_widths = [0.1, 0.5, 1.0, 2.0]
-
-print("\nAdditional HEE calculations:")
-for d in dimensions:
-    print(f"\nResults for d = {d} (AdS{d+1}):")
-    for l in strip_widths:
-        hee = calculate_hee(d, l)
-        print(f"  Strip width = {l}: HEE = {hee:.6f}")
